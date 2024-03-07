8bae4cf6649ad8e73d7c051f72ed1416505a042b
Ho Van Ngan <nganhopro2010@gmail.com>
2024-03-07 23:06:17 +0700

    first commit

---
+++ CostCenterSDK/CostCenterSDK/CostCenterSDK.h
@@ -0,0 +1,18 @@
+//
+//  CostCenterSDK.h
+//  CostCenterSDK
+//
+//  Created by Ho Van Ngan on 07/03/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CostCenterSDK.
+FOUNDATION_EXPORT double CostCenterSDKVersionNumber;
+
+//! Project version string for CostCenterSDK.
+FOUNDATION_EXPORT const unsigned char CostCenterSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CostCenterSDK/PublicHeader.h>
+
+
---
+++ CostCenterSDK/CostCenterSDK/CostCenterSDK.swift
@@ -0,0 +1,14 @@
+//
+//  CostCenterSDK.swift
+//  CostCenterSDK
+//
+//  Created by Ho Van Ngan on 07/03/2024.
+//
+
+import Foundation
+
+public struct CostCenterSDK {
+    public func initialize(){
+        NSLog("CostCenterSDK initialize")
+    }
+}
