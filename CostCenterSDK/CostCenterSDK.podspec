749a16dd47c91efd5a648747135eebcfca2dc0c0
Ho Van Ngan <nganhopro2010@gmail.com>
2024-03-14 00:34:20 +0700

    Add version 1.1

--- CostCenterSDK/CostCenterSDK.podspec
+++ CostCenterSDK/CostCenterSDK.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |spec|
   spec.name          = 'CostCenterSDK'
-  spec.version       = '1.0'
+  spec.version       = '1.1'
   spec.homepage      = 'https://github.com/nganhopro2010/CostCenterSDK'
   spec.authors      = { 'Ho Van Ngan' => 'nganhopro2010@gmail.com' }
   spec.summary       = 'It is a cost center name xcframework example'
--- CostCenterSDK/CostCenterSDK/CostCenterSDK.h
+++ CostCenterSDK/CostCenterSDK/CostCenterSDK.h
@@ -14,6 +14,7 @@ FOUNDATION_EXPORT double CostCenterSDKVersionNumber;
 //! Project version string for CostCenterSDK.
 FOUNDATION_EXPORT const unsigned char CostCenterSDKVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <CostCenterSDK/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like 
+#import <CostCenterSDK/CostCenterSDK.h>
 
 
